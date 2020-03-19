3f7e914284612749a276863c7bf2db3370992fb2
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2020-03-19 13:51:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,13 @@ from FCLayer import BaseLayer, MiddleLayer, OutputLayer
 from mnist_call import mnist_call
 
 
-N=2000
+N=20000
 eta = 0.01
 epoch = 30
-batch_size = 200
-n_sample = 200
+batch_size = 1024
+n_sample = 500
 # 학습데이터 호출
-images_train, images_test, labels_train, labels_test = mnist_call(N)
-
-
-# 사용에 쓰이는 data로 변환완료
-input_train = images_train
-correct_train = labels_train
-input_test = images_test
-correct_test = labels_test
+input_train, input_test, correct_train, correct_test = mnist_call(N)
 
 n_train = input_train.shape[0]
 n_test  = input_test.shape[0]
@@ -32,12 +25,14 @@ img_w = 28
 img_ch = 1
 
 # -- 각 층의 초기화 --
-cl_1 = ConvLayer(img_ch, img_h, img_w, 10, 5, 5, stride = 1, pad = 2) #앞3개:인풋 중간3개:필터
-pl_1 = PoolingLayer(cl_1.y_ch, cl_1.y_h, cl_1.y_w, pool = 2, pad = 0) # pool:풀링 영역 크기, pad:패딩 너비
+cl1 = ConvLayer(img_ch, img_h, img_w, 10, 5, 5, stride = 1, pad = 2) #앞3개:인풋 중간3개:필터
+pl1 = PoolingLayer(cl1.y_ch, cl1.y_h, cl1.y_w, pool = 2, pad = 0) # pool:풀링크기(2*2), pad:패딩 너비
+cl2 = ConvLayer(pl1.y_ch, pl1.y_h, pl1.y_w, 20, 3, 3, stride = 1, pad = 1)
+pl2 = PoolingLayer(cl2.y_ch, cl2.y_h, cl2.y_w, pool = 2, pad = 0)
 
-n_fc_in = pl_1.y_ch * pl_1.y_h * pl_1.y_w
-ml_1 = MiddleLayer(n_fc_in, 100)
-ol_1 = OutputLayer(100, 10)
+n_fc_in = pl2.y_ch * pl2.y_h * pl2.y_w
+ml1 = MiddleLayer(n_fc_in, 1000)
+ol1 = OutputLayer(1000, 10)
 
 
 # -- 순전파--
@@ -45,36 +40,42 @@ def forward_propagation(x):
     n_bt = x.shape[0]
 
     images = x.reshape(n_bt, img_ch, img_h, img_w)
-    cl_1.forward(images)
-    pl_1.forward(cl_1.y)
+    cl1.forward(images)
+    pl1.forward(cl1.y)
 
-    fc_input = pl_1.y.reshape(n_bt, -1)
-    ml_1.forward(fc_input)
-    ol_1.forward(ml_1.y)
+    cl2.forward(pl1.y)
+    pl2.forward(cl2.y)
+
+    fc_input = pl2.y.reshape(n_bt, -1)
+    ml1.forward(fc_input)
+    ol1.forward(ml1.y)
 
 
 # -- 역전파 --
 def backpropagation(t):
     n_bt = t.shape[0]
 
-    ol_1.backward(t)
-    ml_1.backward(ol_1.grad_x)
+    ol1.backward(t)
+    ml1.backward(ol1.grad_x)
 
-    grad_img = ml_1.grad_x.reshape(n_bt, pl_1.y_ch, pl_1.y_h, pl_1.y_w)
-    pl_1.backward(grad_img)
-    cl_1.backward(pl_1.grad_x)
+    grad_img = ml1.grad_x.reshape(n_bt, pl2.y_ch, pl2.y_h, pl2.y_w)
+    pl2.backward(grad_img)
+    cl2.backward(pl2.grad_x)
 
+    pl1.backward(cl2.grad_x)
+    cl1.backward(pl1.grad_x)
 
 # -- 가중치와 편향 수정 --
 def uppdate_wb():
-    cl_1.update(eta)
-    ml_1.update(eta)
-    ol_1.update(eta)
+    cl1.update(eta)
+    cl2.update(eta)
+    ml1.update(eta)
+    ol1.update(eta)
 
 
 # -- 오차 계산 --
 def get_error(t, batch_size):
-    return -np.sum(t * np.log(ol_1.y + 1e-7)) / batch_size  # 교차 엔트로피 오차
+    return -np.sum(t * np.log(ol1.y + 1e-7)) / batch_size  # 교차 엔트로피 오차
 
 
 # -- 샘플을 순전파 --
@@ -105,7 +106,7 @@ for i in range(epoch) :
     # train 정답률측정
     ok, nope = 0, 0
     for k in range(n_sample) :
-        if np.argmax(t[k,:]) == np.argmax(ol_1.y[k,:]) :
+        if np.argmax(t[k,:]) == np.argmax(ol1.y[k,:]) :
             ok = ok + 1
         else :
             nope = nope + 1
@@ -116,7 +117,7 @@ for i in range(epoch) :
     # 테스트 정답률측정
     ok2, nope2 = 0, 0
     for k in range(n_sample):
-        if np.argmax(t[k, :]) == np.argmax(ol_1.y[k, :]):
+        if np.argmax(t[k, :]) == np.argmax(ol1.y[k, :]):
             ok2 = ok2 + 1
         else:
             nope2 = nope2 + 1
